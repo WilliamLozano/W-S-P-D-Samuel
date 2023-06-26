939377c3f78dd3cf79c8cf3fec3073b3285ea69a
William Lozano <lozanozalazarwilliamandres@gmail.com>
2023-06-25 20:49:21 -0500

    Codigo clase oferta actualizado

--- Clase.py
+++ Clase.py
@@ -1,13 +1,23 @@
 class Oferta:
     def __init__(self, Id, vacante, numeropostulados, FechaPublicacion, FechaCierre, CandidatosaIntrevistar, Estado, oferta):
-        self.Id = Id
-        self.vacante = vacante
+        self.Id = Id 
+        self.vacante = vacante 
         self.numeropostulados = numeropostulados
         self.FechaPublicacion = FechaPublicacion
         self.FechaCierre = FechaCierre
-        self.CandidatosaIntrevistar = CandidatosaIntrevistar
+        self.CandidatosaEntrevistar = CandidatosaIntrevistar
         self.Estado = Estado
-        self.oferta = oferta
+        self.oferta = [oferta]
+        self.Id = int(input("Ingrese su ID en el sistema. :"))
+        self.vacante = str(input("Ingrese la vacante al sistema. :")) 
+        self.numeropostulados = int(input("Ingrese el numero de postulados en el sistema. :"))
+        self.FechaPublicacion =(input("Ingrese la fecha de publicación de la oferta en el sistema. :"))
+        self.FechaCierre = (input("Ingrese la fecha de cierre de la oferta en el sistema. :"))
+        self.CandidatosaEntrevistar = int(input("Ingrese el numero de candidatos a entrevistar"))
+        self.Estado = str(input("Ingrese el estado de la oferta en el sistema. :"))
+        self.oferta = str(input("Ingrese la oferta en el sistema. :"))
+
+    
 
     def mostrar_atributos(self):
         while True:
@@ -34,7 +44,7 @@ class Oferta:
             elif opcion == "5":
                 print("Fecha de Cierre:", self.FechaCierre)
             elif opcion == "6":
-                print("Candidatos a Entrevistar:", self.CandidatosaIntrevistar)
+                print("Candidatos a Entrevistar:", self.CandidatosaEntrevistar)
             elif opcion == "7":
                 print("Estado:", self.Estado)
             elif opcion == "8":
@@ -45,5 +55,5 @@ class Oferta:
                 print("Opción inválida")
 
 # Ejemplo de uso
-mi_oferta = Oferta(1, "Desarrollador Python", 10, "2023-06-01", "2023-06-30", 5, "Abierta", ["Beneficios competitivos", "Oportunidad de crecimiento"])
+mi_oferta = Oferta(1,2,3,4,5,6,7,8)
 mi_oferta.mostrar_atributos()
